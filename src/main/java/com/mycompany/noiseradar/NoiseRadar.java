3cc807f22e4eb8b2c6083e9ca19620b9240f5f1d
DESKTOP-OB0TAQ6\user <jhjjang0922@naver.com>
2025-05-18 22:10:59 +0900

    initialize

---
+++ src/main/java/com/mycompany/noiseradar/NoiseRadar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.noiseradar;
+
+/**
+ *
+ * @author user
+ */
+public class NoiseRadar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
